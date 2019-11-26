a7056fad46b23aa0a85c399e66866bbed29dab71
Janaina Silva <janaina.silva@lambda3.com.br>
2019-11-26 11:14:06 -0300

    Tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -10,7 +10,32 @@ const GuessedWord = (props) => {
                 Try to guess the secret word!
             </span>
         )
-    }
+    }else{
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr data-test='guessed-word' key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ))
+
+        contents = (
+            <div data-test='guessed-words'>
+                <h3>Guessed Words</h3>
+                <table>
+                    <thread>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        { guessedWordsRows }
+                    </tbody>
+                </table>
+            </div>
+        )
+    } 
+
     return (
         <div data-test="component-guessed-words">
             {contents}
