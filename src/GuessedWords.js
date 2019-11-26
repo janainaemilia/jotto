f19c23165fe3bb8e0b47ece1c90d3e9ea813e247
Janaina Silva <janaina.silva@lambda3.com.br>
2019-11-26 11:01:52 -0300

    Start tests for GuessedWords

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,15 +2,26 @@ import React from 'react'
 import ProtoTypes from 'prop-types'
 
 const GuessedWord = (props) => {
+    let contents
+
+    if(props.guessedWords.length === 0) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        )
+    }
     return (
-        <div />
+        <div data-test="component-guessed-words">
+            {contents}
+        </div>
     )
 }
 
 GuessedWord.protoTypes = {
     guessedWords: ProtoTypes.arrayOf(
         ProtoTypes.shape({
-            guessedWords: ProtoTypes.string.isRequired,
+            guessedWord: ProtoTypes.string.isRequired,
             letterMatchCount: ProtoTypes.number.isRequired
         })
     ).isRequired,
