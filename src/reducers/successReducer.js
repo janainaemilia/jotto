02988f078f5f20f8f2a0534f10a5f19a45d17b9a
Janaina Silva <janaina.silva@lambda3.com.br>
2019-11-26 12:27:03 -0300

    Checking status

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} object - Action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) => {
+    return null
+}
