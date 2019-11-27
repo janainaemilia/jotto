71370249155e55779845995c36821f4761048737
Janaina Silva <janaina.silva@lambda3.com.br>
2019-11-27 11:50:27 -0300

    Add Thunk for `guessedWords` piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,29 @@
+import { getLetterMatchCount } from '../helpers'
 
 export const actionTypes = {
-    CORRET_GUESS: 'CORRET_GUESS',
+    CORRECT_GUESS: 'CORRECT_GUESS',
     GUESS_WORD: 'GUESS_WORD',
 }
 
 /**
  * Returns Redux Thunk function that dispatches GUESS_WORD action
  *          and (conditionaly) CORRECT_GUESS action
- * @function guessedWord
+ * @function guessWord
  * @param {string} guessedWord - Guessed word.
  * @returns {function} - Redux Thunk function.
  */
-export const guessedWord = (guessedWord) => {
-    return function(dispatcher, getState) {
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+        const secretWord = getState().secretWord
+        const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)
 
+        dispatch({
+            type: actionTypes.GUESS_WORD,
+            payload: { guessedWord, letterMatchCount }
+        })
+
+        if(guessedWord === secretWord){
+            dispatch({ type: actionTypes.CORRECT_GUESS })
+        }
     }
 }
---
+++ src/reducers/secretWordReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function secretWordReducer
+ * @param {array} state= Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {array} - new guessedWords state.
+ */
+export default (state=null, action) => {
+    return state
+}
